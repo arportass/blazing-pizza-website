3223423fee5a0d1a6f2ec5478b13223ac2559d54
arportas <arturo.portas.s@outlook.com>
2023-08-27 14:00:08 -0600

    Preparing Welcome Page.

---
+++ Data/PizzaSpecial.cs
@@ -0,0 +1,20 @@
+namespace BlazingPizza
+{
+    /// <summary>
+    /// Represents a special offer of a pizza that can be ordered.
+    /// </summary>
+    public class PizzaSpecial
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal BasePrice { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string GetFormattedBasePrice() => BasePrice.ToString("0.00");
+    }
+}
